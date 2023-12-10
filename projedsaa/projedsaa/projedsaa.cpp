7874bb27382ca16e27f8ae1d4e07b229c030562a
Emir <62707230+thecloudrazor@users.noreply.github.com>
2023-12-10 16:53:13 +0300

    Some edits about the code comments

--- projedsaa/projedsaa/projedsaa.cpp
+++ projedsaa/projedsaa/projedsaa.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 using namespace std::chrono;
 
-string RSN(string HHH) { //RSN = read and return site name
+string RSN(string HHH) { // RSN = read and return site name
 
 	int start, end;
 	start = HHH.find("GET") + 4;
@@ -22,31 +22,31 @@ string RSN(string HHH) { //RSN = read and return site name
 
 int main(){
 
-	ifstream log("access_log.txt");           //this is our way to open the text file [line 42]
+	ifstream log("access_log.txt");          // This is our way to open the text file [line 42]
 
-	string check                             // check is to get sites name, since I use it multiple times I made it equal to a value [line 43 to 45]
-		  ,line;                             // line is to get the lines of the text file,  [line 42 and 43]
+	string check                             // "check" is to get site names, since it is used multiple times, it's been made equal to a value [line 43 to 45]
+		  ,line;                             // "line" is to get the lines of the text file [line 42 and 43]
 
-	unordered_map<string, int> STK;           // STK = string to keyvalue / Im using this to store the amouth of visits and names of each site [line 43]
-	unordered_map<int , vector<string> > KTS; // KTS = keyvalue to string / Im using this to order the values            
+	unordered_map<string, int> STK;           // STK = string to keyvalue / this is used to store the amount of visits and names of each site [line 43]
+	unordered_map<int , vector<string> > KTS; // KTS = keyvalue to string / this is used to order the values 
 
-	int xc =0                                 // xc is used to find the biggest value, only print out 10 numbers (our break condition) [line 48 to 82]  
+	int xc =0                                 // xc is used to find the biggest value, only print out 10 numbers (our break condition) [line 48 to 82] 
 	   ,kc=0								  // kc is there to check all values in our map [line 48 to 82]
-	   ,x=0                                   // x is thre so we dont check more times that line in our text file (less than 10 sites) [line 45,49,80]
-	   ,a=0;                                  // a is used so that we dont print same site names since we store allnames from 1 to n for each site [line 59,63,71]
+	   ,x=0                                   // x is there so we don't check more times that line in our text file (less than 10 sites) [line 45,49,80]
+	   ,a=0;                                  // a is used to ensure that the same site names are not printed, since we store all names from 1 to n for each site [line 59,63,71]
 
-	string checkA[10];                        //this array is used so that we dont print same site names since we store all names from 1 to n for each site [line 57 and 66]
+	string checkA[10];                        // this array is used to ensure that the same site names are not printed, since all names are stored from 1 to n for each site [line 57 and 66]
 
-	high_resolution_clock::time_point time1 = high_resolution_clock::now(); //this is for taking the time
+	high_resolution_clock::time_point time1 = high_resolution_clock::now(); // this is for taking the time
 	   
-		while (getline(log, line)) {         // this while is to put my values to my unordered maps
+		while (getline(log, line)) {         // this while is to put the values to the unordered maps
 			check = RSN(line);
 			STK[check]++;
 			KTS[STK[check]].push_back(check);
 			x++;
 	    }                                     
 		
-		while (1) {                           //this while is for printing top 10 visited sites
+		while (1) {                           // this while is for printing top 10 visited sites
 			if (kc == x ) {					 
 				std::cout << "theres less than 10 sites"<<endl;
 				break;
