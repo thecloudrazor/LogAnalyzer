293500b39446b6db11fd6e0ec79c153c180d0509
Emir <62707230+thecloudrazor@users.noreply.github.com>
2023-12-10 19:14:46 +0300

    Son güncellemeler
    Comment değişiklikleri ve variable değişiklikleri

--- projedsaa/projedsaa/projedsaa.cpp
+++ projedsaa/projedsaa/projedsaa.cpp
@@ -9,12 +9,12 @@
 using namespace std;
 using namespace std::chrono;
 
-string RSN(string HHH) { // RSN = read and return site names
+string RSN(string linex) { // RSN = read and return site names
 
 	int start, end;
-	start = HHH.find("GET") + 4;
-	end = HHH.find("HTTP", start) - 1;
-	return HHH.substr(start, end - start);
+	start = linex.find("GET") + 4;
+	end = linex.find("HTTP", start) - 1;
+	return linex.substr(start, end - start);
 
 }
 
@@ -24,7 +24,7 @@ int main(){
 
 	ifstream log("access_log.txt");          // This is our way to open the text file [line 42]
 
-	string checkS                             // "checkS" is to get site names (it's been made equal to a value since it is utilized multiple times by the program) [line 43 to 45]
+	string checkS                            // "checkS" is to get site names (it's been made equal to a value since it is utilized multiple times by the program) [line 43 to 45]
 		  ,line;                             // "line" is to get the lines of the text file [line 42 and 43]
 
 	unordered_map<string, int> STK;           // STK = string to keyvalue / this is used to store the amount of visits and names of each site [line 43]
@@ -33,22 +33,22 @@ int main(){
 	int tenCounter=0                          // "tenCounter" is used to find the biggest value, only print out 10 numbers (the break condition) [line 48 to 82] 
 	   ,counter=0							  // "counter" is used to check all values in our map [line 48 to 82]
 	   ,checkI=0                              // "checkI" is used to ensure checking is not done than more times the amount of lines in our text file (less than 10 sites) [line 45,49,80]
-	   ,ensurer=0;                                  // "ensurer" is used to ensure that the same site names are not printed, since all names are stored from 1 to n for each site [line 59,63,71]
+	   ,ensurer=0;                            // "ensurer" is used to ensure that the same site names are not printed, since all names are stored from 1 to n for each site [line 59,63,71]
 
 	string checkA[10];                        // This array is used to ensure that the same site names are not printed, since all names are stored from 1 to n for each site [line 57 and 66]
 
 	high_resolution_clock::time_point time1 = high_resolution_clock::now(); // This is used for taking the time
 	   
 		while (getline(log, line)) {         // This while loop is used to put the values to the unordered maps
-			check = RSN(line);
-			STK[check]++;
-			KTS[STK[check]].push_back(check);
+			checkS = RSN(line);
+			STK[checkS]++;
+			KTS[STK[checkS]].push_back(checkS);
 			checkI++;
 	    }                                     
 		
 		while (1) {                           // This while loop is used to print top 10 visited sites
 			if (counter == checkI ) {					 
-				std::cout << "theres less than 10 sites"<<endl;
+				std::cout << "There's less than 10 sites"<<endl;
 				break;
 			}
 			else if  (KTS[counter + 1].size() == tenCounter) { 
@@ -81,8 +81,8 @@ int main(){
 			
 		}
 		
-		high_resolution_clock::time_point time2 = high_resolution_clock::now(); // This is here to check time
-		duration<double> time_span = duration_cast<duration<double>>(time2 - time1); // This is here to check time
-		std::cout<<endl<< "Total Elapsed Time : " << time_span.count()<<"Seconds"<<endl; // This is here to print time
+		high_resolution_clock::time_point time2 = high_resolution_clock::now(); 			// This is here to check time
+		duration<double> time_span = duration_cast<duration<double>>(time2 - time1); 		// This is here to check time
+		std::cout<<endl<< "Total Elapsed Time: " << time_span.count()<<"Seconds"<<endl; 	// This is here to print time
 		return 0;
 }
