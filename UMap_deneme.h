169f769e8f11aa24bb68a60cc46b1261b1cfca10
dmrv0 <erogludemir06@gmail.com>
2023-12-14 12:43:13 +0300

    OO taslak denemesi
    basit object oriented düzenlemesi ( main dosyadaki yazıları okuyun)

---
+++ UMap_deneme.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <unordered_map>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+class UMap {  // class for Unordered_Map Solution (Hash map classý açýldýktan sonra inherit edilebilir).
+public:
+    UMap(); // Definition of default constructor.
+    string read_logs(string linex); // Definition of the function that reads the file names from "acces_log.txt". 
+    void to_map();
+    void transfer_sort();
+    void print_topten();
+    void one_cycle();
+private:
+    ifstream logfile; // this will be our access_log
+    unordered_map<string, int> visitCounts; // this will hold the information about which log visited how many times.
+    vector<pair<int, string>> visitVector; // this will be the vector to get sorted at the end.
+};
+
+
